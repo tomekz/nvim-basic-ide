29cc216a908458eae71799be5c613c348ff87835
tomekz <zadrozny.tomasz@gmail.com>
2022-09-28 13:30:42 +0200

    local config adaptations

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -97,11 +97,11 @@ cmp.setup({
 		format = function(entry, vim_item)
 			vim_item.kind = kind_icons[vim_item.kind]
 			vim_item.menu = ({
-				nvim_lsp = "",
+				nvim_lsp = "[LSP]",
 				nvim_lua = "",
-				luasnip = "",
-				buffer = "",
-				path = "",
+				luasnip = "[Snippet]",
+				buffer = "[Buffer]",
+				path = "[Path]",
 				emoji = "",
 			})[entry.source.name]
 			return vim_item
